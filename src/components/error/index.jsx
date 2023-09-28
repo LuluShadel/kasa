1b78b0753c9c1ef70d1dedc015e3459d4398081b
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2023-09-28 16:05:17 +0200

    Intégration de la page d'erreur

--- src/components/error/index.jsx
+++ src/components/error/index.jsx
@@ -1,7 +1,37 @@
+import styled from "styled-components"
+import { Link } from "react-router-dom"
+import colors from "../../styles/colors"
+
+
+//style 
+
+const StyledH1 = styled.h1`
+    font-size:15em;
+    color:${colors.primary};
+    text-align:center;
+`
+
+const StyledH2 = styled.h2 `
+color: ${colors.primary};
+font-size:3em;
+text-align:center;
+`
+
+const StyledLink = styled(Link) `
+color:${colors.primary};
+display: flex;
+justify-content: center;
+margin-bottom:100px;
+margin-top:100px
+`
+
+
 function Error () {
     return(
         <div>
-            <h1>ERROR</h1>
+            <StyledH1>404</StyledH1>
+            <StyledH2>Oups! La page que vous demandez n'existe pas.</StyledH2>
+            <StyledLink to="/">Retourner sur la page d'accueil</StyledLink>
         </div>
     )
 }
