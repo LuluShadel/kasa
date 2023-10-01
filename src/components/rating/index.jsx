aa5f37279f30b99c1f9d7c0c2429d6846f56dae3
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2023-10-01 21:24:22 +0200

    ajout de la fonction rating et du style

--- src/components/rating/index.jsx
+++ src/components/rating/index.jsx
@@ -1,9 +1,23 @@
+import "./rating.scss"
+
+
 function Rating (props) {
+    const array = [1,2,3,4,5];
+
+    const rating_note = props.rating
 
     return (
-        <div>
-            <div>{props.rating}</div>
+        <div className="rating"> 
+            <div className="neutral_star">{array.map((rangeRating) =>
+            <span key={rangeRating.toString()}><i className="fa-solid fa-star"></i></span>)}
+            </div>
+            <div className="show_star">
+            {array.map((rangeRating) =>
+            rating_note >= rangeRating ? <span key={rangeRating.toString()}><i className="fa-solid fa-star"></i></span> : null )}
+       </div>
+       
         </div>
+    
     )
 }
 
